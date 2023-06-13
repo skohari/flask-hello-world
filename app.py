f55ec7a77b212b827ba31a4f9b5e4cff0877dcac
s kohari <skohari@gmail.com>
2023-06-13 18:06:15 -0500

    Add time stamp

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/hw')
+def index():
     return 'Hello, World! 3'
+
+
+@app.route('/')
+def greet():
+    time = datetime.now().hour
+    if time >= 0 and time < 12:
+        return 'Good Morning!'
+    elif time >= 12 and time < 16:
+        return 'Good Afternoon!'
+    else:
+        return 'Good Evening!'
